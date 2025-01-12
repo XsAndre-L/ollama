55cd0d526c729a51134c4ee158cf4fcd37a386ee
xsandre-l <ahloubser12@gmail.com>
2025-01-12 15:59:33 +0200

    .

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ make -j12 exe
 
 #  docker build -t ollama .
 
-# docker run --gpus all -p 11434:11434 -v "C:/Users/ahlou/.ollama:/root/.ollama" ollama
+# docker run --gpus all -p 11434:11434 -v "C:/Users/ahlou/.ollama:/root/.ollama" --name ollama ollama
 
 # "C:/Users/ahlou/.ollama:/root/.ollama" is the path to the directory where the .ollama directory is located on the host machine. This directory contains the configuration files for the Ollama tool.
